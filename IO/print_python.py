8056f110ccf686cb2d728605748556a90d284ad3
SalahEddineGhamri <salaheddine.ghamri@yahoo.com>
2018-10-29 21:34:06 +0100

    go!

---
+++ IO/print_python.py
@@ -0,0 +1,13 @@
+# this script shows all print options in python
+# author: Salah Eddine Ghamri
+
+# *-- print a string
+print(" Hello, world !")
+print(" Hello," + " world !")
+print(" %s " % "Hello, world !")
+print(" {} ".format("Hello, world !"))
+#*-----
+print(" Hello,", end='')
+print(" world !")
+#-------
+print("This is ", 2**32, ", it's a magical number.", sep="-*****-")
