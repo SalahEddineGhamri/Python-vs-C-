1ec9c5cdbfc665aefbea2b9f559899bb85e1a204
salaheddineghamri <salaheddine.ghamri@yahoo.com>
2018-11-03 17:30:36 +0100

    fstream istream ostrea

---
+++ File_IO/fileIO_fstream.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int nbr = 1, nbr1 = 0, nbr2 = 0, result=0;
+int main()
+{
+    fstream file("The_file.txt", ios::in | ios::out);
+    string line;
+    if( file.is_open()){
+        while (getline(file, line))
+        {
+            cout << nbr << " " << line << endl;
+            if( nbr == 2 ){ nbr1 = stoi(line);}
+            else if(nbr == 4 ){ nbr2 = stoi(line);}
+            else if(nbr == 5 ){
+                result = nbr1 * nbr2;
+                file << result;
+            }
+            cout << result << endl;
+            nbr += 1;
+        }
+    }else{ cout << " Errors in opening file "<< endl; }
+    file.close();
+    return 0;
+}
--- File_IO/test.cpp
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-using namespace std;
-
-int main(){
-    printf("This is a number %d but i am not sure\n", 99);
-    return 0;
-}
