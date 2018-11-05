195618230bf06b964ab039b60a881bfdc7554e44
SalahEddineGhamri <salaheddine.ghamri@yahoo.com>
2018-11-05 21:26:22 +0100

    hhh

--- loops/loops.cpp
+++ loops/loops.cpp
@@ -4,37 +4,38 @@ using namespace std;
 int main(){
 
     // for loop
-    /*
+
     for(int i=0; i<10; i++){
         printf("for now is %d\t", i);
-    }*/
-    /*
+    }
+    printf("\n");
     // while loop
     int i = 0;
     while( i<=10){
         printf("while now is %d\t", i);
         i += 1;
-    }*/
+    }
+    printf("\n");
 
     // while...do
-    /*
-    int i = 0;
+    i = 0;
     do{
-        printf("while do now is %d\t", i);
+        printf("whiledo now is %d\t", i);
         i+=1;
-    }while(i <= 10);*/
+    }while(i < 8);
+    printf("\n");
 
     // playing with continue and break
-    int i = 0;
+    i = 0;
     while(i<10){
         i+=1;
         if( i==4 ){
             continue;
         }
-        cout << i << endl;
+        cout << i << "\t";
         if( i==5 )
             break;
     }
-
+    cout<<endl;
     return 0;
 }
