9de91c1043798b3034ed59ee4643ba8f2ee88195
salaheddineghamri <salaheddine.ghamri@yahoo.com>
2018-11-06 22:07:17 +0100

    object pointer dynamic allocation

--- OOP/Main.cpp
+++ OOP/Main.cpp
@@ -2,9 +2,23 @@
 #include "humanClass.h"
 
 int main(){
+    // Direct declaration
     human Salah( "Salah", 30);
     human SalahCopy(Salah);
     SalahCopy.sayName();
     SalahCopy.sayAge();
+    // Declaration through dynamic allocation
+    human *Eddine = new human("Eddine", 45);
+    Eddine ->sayAge();
+    Eddine ->sayName();
+    // Pointer of a class
+    human *ptrHuman;
+    ptrHuman = &Salah;
+    ptrHuman ->sayName();
+    ptrHuman ->sayAge();
+    //
+    human *ptrHuman2 = Eddine;
+    ptrHuman2 ->sayName();
+    ptrHuman2 ->sayAge();
     return 0;
 }
