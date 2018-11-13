208d694320ac16877bea06ca02a4af304ec3847e
salaheddineghamri <salaheddine.ghamri@yahoo.com>
2018-11-13 01:02:12 +0100

    vectors

---
+++ dict/STL_library/vectors.cpp
@@ -0,0 +1,33 @@
+#include<vector>
+#include<iostream>
+#include<string>
+using namespace std;
+
+vector<string> names;
+
+int main(){
+    names.push_back("Salah");
+    names.push_back("Eddine");
+    names.push_back("Ghamri");
+
+    cout << names.size() << endl;
+    // like array
+    for (int i=0 ; i < 3; ++i){
+        cout << i << ' ';
+        cout << names[i] << endl;
+    }
+    // like python list
+    for (auto element = names.begin(); element != names.end(); ++element)
+        cout << *element << endl;
+    
+    // front and back
+    cout << names.front() << endl; // a reference
+    cout << names.back() << endl; // a reference
+
+    // point to the firt element
+    string * pos = names.data();
+    cout << *pos << ' ';
+    cout << *++pos <<endl;
+
+    return EXIT_SUCCESS;
+}
---
+++ dict/dictionary.py
@@ -0,0 +1,6 @@
+#declarations of dictionnary
+dict1 = { 0 : 'A', 1: 'B', 2:'C'}
+dict2 = { 'A' : 1, 'B': 2, 'C':3}
+dict3 = { i:'A' for i in range(5)}
+dict4 = { 0:[0, 1], 1:['A', 'B'], 2:[True, False], 3:[]}
+print(dict4)
